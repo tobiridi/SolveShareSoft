d086a8b9af6c7afddff24590838d07e1675c61f9
tobiridi <tony.6141@hotmail.com>
2024-05-30 14:23:30 +0200

    [FIX] author commit

--- SolveShareSoft-API/app.js
+++ SolveShareSoft-API/app.js
@@ -18,7 +18,6 @@ app.use('/api/v1', router);
 
 const port = process.env.PORT || 3000;
 
-
 app.listen(port, () => {
     console.log(`API Server is running on : http://localhost:${port}/api/v1/`);
 });
