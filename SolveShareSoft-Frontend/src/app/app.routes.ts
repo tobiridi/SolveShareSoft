2441db00a8d7ad26a475ec14bf121d972b07982c
tobiridi <tony.6141@hotmail.com>
2024-06-05 14:55:48 +0200

    [ADD] minimal softwares page

--- SolveShareSoft-Frontend/src/app/app.routes.ts
+++ SolveShareSoft-Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SoftwaresComponent } from './softwares/softwares.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'softwares', component: SoftwaresComponent},
 ];
---
+++ SolveShareSoft-Frontend/src/app/softwares/softwares.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-softwares',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './softwares.component.html',
+  styleUrl: './softwares.component.scss'
+})
+export class SoftwaresComponent {
+
+}
