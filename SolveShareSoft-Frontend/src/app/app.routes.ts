08c98b594c7d6bd2e222252e807e5cacf37da0bf
tobiridi <tony.6141@hotmail.com>
2024-06-05 15:24:20 +0200

    [ADD] login + register page

--- SolveShareSoft-Frontend/src/app/app.routes.ts
+++ SolveShareSoft-Frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SoftwaresComponent } from './softwares/softwares.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'softwares', component: SoftwaresComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'login', component: LoginComponent},
 ];
