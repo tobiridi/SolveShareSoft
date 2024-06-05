045806653b1fa946b983bf68578357f44d1e9bfe
tobiridi <tony.6141@hotmail.com>
2024-06-05 16:36:01 +0200

    [ADD] custom error page

--- SolveShareSoft-Frontend/src/app/app.routes.ts
+++ SolveShareSoft-Frontend/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { SoftwaresComponent } from './softwares/softwares.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'softwares', component: SoftwaresComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
+    
+    //must be last !!!
+    {path: '**', component: ErrorpageComponent},
 ];
