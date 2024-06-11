bc4e62928c09ea2d29dfd752c565c89d9348c02f
tobiridi <tony.6141@hotmail.com>
2024-06-11 11:01:44 +0200

    [ADD] softwares sidebar

--- SolveShareSoft-Frontend/src/app/softwares/softwares.component.ts
+++ SolveShareSoft-Frontend/src/app/softwares/softwares.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-softwares',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, SidebarComponent],
   templateUrl: './softwares.component.html',
   styleUrl: './softwares.component.scss'
 })
