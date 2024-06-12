3e6bb9350542c211409b9581db776fe0a71ff45b
tobiridi <tony.6141@hotmail.com>
2024-06-12 15:29:39 +0200

    [IMP] global style

--- SolveShareSoft-Frontend/src/app/app.component.ts
+++ SolveShareSoft-Frontend/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, NavbarComponent]
 })
 export class AppComponent {
   
