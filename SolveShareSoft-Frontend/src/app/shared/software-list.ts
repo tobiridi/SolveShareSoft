9df75f6730c258034e1d93f16f7d8d0193628ade
tobiridi <tony.6141@hotmail.com>
2024-06-12 22:50:20 +0200

    [ADD] user + software list + category interfaces

---
+++ SolveShareSoft-Frontend/src/app/shared/software-list.ts
@@ -0,0 +1,14 @@
+import { Category } from "./category";
+import { User } from "./user";
+
+export interface SoftwareList {
+    softwareListId: number;
+    title: string;
+    description: string;
+    created: Date;
+    lastUpdate: Date;
+    isPublic: boolean;
+    nbrViews: number;
+    category: Category;
+    owner: User;
+}
---
+++ SolveShareSoft-Frontend/src/app/shared/user.ts
@@ -0,0 +1,17 @@
+export interface User {
+    userId: string;
+    email?: string;
+    password?: string;
+    username: string;
+    website?: string;
+    pays?: string;
+    biography?: string;
+    picture?: any;
+    role?: string;
+    status?: string;
+    lastConnection?: Date;
+    register?: Date;
+    lastUpdate?: Date;
+    blockPeriod?: Date;
+    blockMotif?: string;
+}
