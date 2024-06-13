5f4543e7bdf314d360ca09e65ff6ca9406b9fe41
tobiridi <tony.6141@hotmail.com>
2024-06-13 16:17:03 +0200

    [IMP] softwares page style

--- SolveShareSoft-Frontend/src/app/softwares/softwares.component.ts
+++ SolveShareSoft-Frontend/src/app/softwares/softwares.component.ts
@@ -6,22 +6,27 @@ import { Subscription } from 'rxjs';
 import { SoftwarelistService } from '../services/softwarelist.service';
 import { AlertService } from '../services/alert.service';
 import { Category } from '../shared/category';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-softwares',
   standalone: true,
-  imports: [NavbarComponent, SidebarComponent],
+  imports: [NavbarComponent, SidebarComponent, DatePipe, RouterLink],
   templateUrl: './softwares.component.html',
   styleUrl: './softwares.component.scss'
 })
 export class SoftwaresComponent implements OnInit, OnDestroy {
   public allPublicSoftList: SoftwareList[];
+  public newsSoftList: SoftwareList[];
   public allCategories: Category[];
   private sub: Subscription;
 
   constructor(private readonly _softList: SoftwarelistService, private readonly _alert: AlertService) {
     this.sub = new Subscription();
     this.allPublicSoftList = [];
+    //TODO: get all news soft list
+    this.newsSoftList = [];
     //TODO: get all category
     this.allCategories = [];
   }
