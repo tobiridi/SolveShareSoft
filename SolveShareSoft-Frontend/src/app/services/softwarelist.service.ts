256fcac7dabe341a403a492cd7d14ca09e7f4636
tobiridi <tony.6141@hotmail.com>
2024-06-17 14:41:15 +0200

    [ADD] frontend get software list's softwares

--- SolveShareSoft-Frontend/src/app/services/softwarelist.service.ts
+++ SolveShareSoft-Frontend/src/app/services/softwarelist.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { SoftwareList } from '../shared/software-list';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment.development';
-import { Observable, map } from 'rxjs';
+import { Observable, flatMap, map } from 'rxjs';
 import { Category } from '../shared/category';
 import { User } from '../shared/user';
+import { Software } from '../shared/software';
 
 @Injectable({
   providedIn: 'root'
@@ -63,6 +64,60 @@ export class SoftwarelistService {
         });
         
         return allPublicSoftList;
+      }));
+  }
+
+  public getSoftwaresSoftList(softListId: number): Observable<SoftwareList | undefined> {
+    return this._httpClient.get<any>(`${this.apiURL}/${softListId}/software`)
+      .pipe(map((value) => {
+        const data = value.data;
+        let lastListId: number = 0;
+        let userSoftList: SoftwareList | undefined = undefined;
+
+        data.forEach((element: any) => {
+          if(lastListId !== element.softwarelist_id) {
+            lastListId = element.softwarelist_id;
+
+            const cat: Category = {
+              categoryId: element.category_id,
+              name: element.category_name
+            };
+  
+            const softList: SoftwareList = {
+              category: cat,
+              created: element.created,
+              description: element.softList_desc,
+              isPublic: true,
+              lastUpdate: element.softList_last_upd,
+              nbrViews: element.nbr_views,
+              softwareListId: element.softwarelist_id,
+              title: element.title,
+              softwares: []
+            };
+
+            userSoftList = softList;
+          }
+
+          if(element.software_id !== null && userSoftList) {
+              const soft: Software = {
+                softwareId: element.software_id,
+                softwareList: userSoftList,
+                name: element.name,
+                lang: element.lang,
+                link: element.link,
+                nbrDownloads: element.nbr_downloads,
+                size: element.size,
+                sizeUnit: element.size_unit,
+                description: element.soft_desc,
+                lastUpdate: element.soft_last_upd,
+                version: element.version
+              };
+
+              userSoftList.softwares.push(soft);
+          }
+        });
+        
+        return userSoftList;
       }));
   }
 
--- SolveShareSoft-Frontend/src/app/soft-list-details/soft-list-details.component.ts
+++ SolveShareSoft-Frontend/src/app/soft-list-details/soft-list-details.component.ts
@@ -3,11 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 import { SoftwarelistService } from '../services/softwarelist.service';
 import { SoftwareList } from '../shared/software-list';
 import { Subscription } from 'rxjs';
+import { AlertService } from '../services/alert.service';
+import { DatePipe, DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-soft-list-details',
   standalone: true,
-  imports: [],
+  imports: [DatePipe, DecimalPipe],
   templateUrl: './soft-list-details.component.html',
   styleUrl: './soft-list-details.component.scss'
 })
@@ -16,18 +18,34 @@ export class SoftListDetailsComponent implements OnInit, OnDestroy {
   private sub: Subscription;
   private softListId: number;
 
-  constructor(private readonly _activedRoute: ActivatedRoute, private readonly _softList: SoftwarelistService) {
+  constructor(private readonly _activedRoute: ActivatedRoute,
+    private readonly _softList: SoftwarelistService, private readonly _alert: AlertService) {
+
     this.sub = new Subscription();
     this.softListId = this._activedRoute.snapshot.params ["softListId"];
   }
 
   ngOnInit(): void {
-    //TODO: get softwares from the soft list
-      //const getDate: Subscription = this._softList.
+      const getSoftwaresSub: Subscription = this._softList.getSoftwaresSoftList(this.softListId).subscribe({
+        next: (value: SoftwareList | undefined) => {
+          this.currentSoftList = value;
+        },
+        error: (err) => {
+          console.error(err);
+          this._alert.displayAlert('Erreur lors du chargement du details', 'error');
+        },
+      });
+
+      this.sub.add(getSoftwaresSub);
   }
 
   ngOnDestroy(): void {
       this.sub.unsubscribe();
+  }
+
+  public deleteSoft(softId: number): void {
+    //TODO: delete software
+    console.log(softId);
   }
 
 }
